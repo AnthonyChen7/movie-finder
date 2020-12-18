ae8d975b704fb6023d3d36248da3214e6678791f
AnthonyChen7 <hp_a1700@hotmail.com>
2020-12-18 14:09:52 -0800

    got regex and error validation working

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,14 +1,14 @@
 import { useState, ChangeEvent, useEffect } from 'react';
 import './search-bar.scss';
 
-import { Input, Button, Icon } from 'semantic-ui-react';
+import { Input, Button } from 'semantic-ui-react';
 import { useDebouncedCallback } from 'use-debounce';
 
 import 'semantic-ui-css/semantic.min.css';
 
 interface SearchProps {
   defaultValue?: string;
-  regexValidations?: {[key: string]: string};
+  regexValidation?: string;
   placeholder?: string;
   disabled?: boolean;
   searchPressed?: (searchValue: string) => void;
@@ -16,8 +16,16 @@ interface SearchProps {
 
 export default function SearchBar(props: SearchProps) {
   const [ currValue, setCurrValue ] = useState(props.defaultValue ? props.defaultValue : '');
-  const [ errorMessage, setErrorMessage ] = useState('');
-  const [ isDisabled, setDisabled ] = useState(props.disabled);
+  const [ hasError, setHasError ] = useState(false);
+
+  useEffect(() => {
+    if (props.regexValidation) {
+      const regex = new RegExp(props.regexValidation);
+      setHasError(!regex.test(currValue));
+    }
+  }, [
+    props.regexValidation, currValue
+  ]);
 
   // https://github.com/xnimorz/use-debounce
   // use debounce to prevent it from re-rendering on every keypress
@@ -25,28 +33,13 @@ export default function SearchBar(props: SearchProps) {
     // function
     (value: string) => {
       setCurrValue(value);
-      if (props.regexValidations && Object.keys(props.regexValidations).length > 0) {
-        const keys = Object.keys(props.regexValidations);
-        let hasError = false;
-        for (const key of keys) {
-          const regex = new RegExp(key);
-          if (!regex.test(value)) {
-            setErrorMessage(props.regexValidations[key]);
-            hasError = true;
-            break;
-          }
-        }
-        if (!hasError) {
-          setErrorMessage('');
-        }
-      }
     },
     // delay in ms
     500
   );
 
   const onSearchClicked = () => {
-    if (props.searchPressed && !errorMessage) {
+    if (props.searchPressed && !props.disabled) {
       props.searchPressed(currValue);
     }
   };
@@ -57,15 +50,15 @@ export default function SearchBar(props: SearchProps) {
         action={
           <Button
             icon='search'
-            disabled={isDisabled}
+            disabled={props.disabled || hasError}
             onClick={onSearchClicked}
             circular
           >
           </Button>
         }
         fluid
-        error={errorMessage !== ''}
-        disabled={isDisabled}
+        error={hasError}
+        disabled={props.disabled}
         defaultValue={props.defaultValue || ``}
         placeholder={props.placeholder || `Search...`}
         onChange={(event: ChangeEvent, data: any)=> debounced.callback(data.value)}
