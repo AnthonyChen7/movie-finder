31f1cf5f3688fe9f9d82687a00448f6da3760931
AnthonyChen7 <hp_a1700@hotmail.com>
2020-12-18 15:07:35 -0800

    Can fetch dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 import SearchBar from './components/search-bar/search-bar';
+import { Movie } from './models/movie';
+
+const API_KEY = 'http://www.omdbapi.com/?i=tt3896198&apikey=aeb03ac3';
 
 function App() {
+  const [movies, setMovies] = useState([] as Movie[]);
+  const onSearchPressed = async (searchVal: string) => {
+    const response = await fetch(`batman.json`);
+    const json = await response.json();
+    const parsedJson = parseJson(json);
+    setMovies(parsedJson);
+    console.log(json);
+  };
+
+  const parseJson = (json: any) => {
+    const result: Movie[] = [];
+    if (json && json.Search) {
+      const searchResults = json.Search;
+      for (const searchResult of searchResults) {
+        const movie: Movie = {
+          name: searchResult.Title,
+          releasedDate: searchResult.Year
+        };
+        result.push(movie);
+      }
+    }
+    return result;
+  };
   return (
     <div className="App">
       <SearchBar
         disabled={false}
-        searchPressed={(searchVal) => { console.log(searchVal) }}
+        searchPressed={onSearchPressed}
       />
+      {JSON.stringify(movies)}
     </div>
   );
 }
---
+++ src/models/movie.ts
@@ -0,0 +1,6 @@
+export interface Movie {
+  name: string;
+  releasedDate: string;
+  // in minutes
+  // duration: string;
+};
